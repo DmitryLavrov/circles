506fd8cbdbc37cd82e60d94486d0fff64e01fe8d
DmitryLavrov <dimlavdev@gmail.com>
2023-03-29 19:54:30 +0300

    Add redirect to /circle

--- src/processes/rootes/routes.tsx
+++ src/processes/rootes/routes.tsx
@@ -1,4 +1,4 @@
-import {RouteObject} from 'react-router-dom'
+import {RouteObject, Navigate} from 'react-router-dom'
 import {paths} from './paths'
 import React from 'react'
 import {Home} from 'pages/home/ui'
@@ -7,4 +7,5 @@ import {Menu} from 'pages/menu/ui'
 export const routes: RouteObject[] = [
   {path: paths.home, element: <Home/>},
   {path: paths.menu, element: <Menu/>},
+  {path: '*', element:  <Navigate to={paths.home}/>}
 ]
