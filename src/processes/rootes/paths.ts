4eda53891f364b7bc35103b67fa52d30a94fc6d3
DmitryLavrov <dimlavdev@gmail.com>
2023-03-31 11:22:08 +0300

    Update path.other

--- src/processes/rootes/paths.ts
+++ src/processes/rootes/paths.ts
@@ -1,10 +1,7 @@
 import {BASE_URL} from './const'
 
-//--------------------------
-console.log('BASE_URL',BASE_URL)
-//--------------------------
-
 export const paths = {
   home: BASE_URL + '/',
   menu: BASE_URL + '/menu',
+  other: BASE_URL + '/*',
 }
